cb8b83f1e017a01c545e2259e0e9e89b7a4a7369
Crowdbotics <team@crowdbotics.com>
2022-08-22 16:55:46 +0000

    Initial commit.

---
+++ backend/shammir_test_36859/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shammir_test_36859 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shammir_test_36859.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shammir_test_36859",
+  url: "https://shammir_test_36859.botics.co",
+  api: "https://shammir_test_36859.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
